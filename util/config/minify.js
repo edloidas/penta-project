b63cf156d56992b9a61cdaef4982d3bce59c5108
edloidas <edloidas@gmail.com>
2016-11-04 12:22:49 +0300

    Update webpack config.
    Updated main html file
    Added pug support to webpack
    Added basic babel support to clean js files from flow types.

---
+++ util/config/minify.js
@@ -0,0 +1,11 @@
+// html-minifier options
+module.exports = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  html5: true,
+  preserveLineBreaks: true,
+  removeComments: true,
+  removeEmptyAttributes: true,
+  useShortDoctype: true,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,77 @@
 /*
-Default webpack configureation.
-.Will be used, when runnnig webpack from the cli.
+Configurable webpack config.
+Uses $NODE_ENV, `production` or `development`
 */
-const webpackMultiConfig = require( './util/webpackMultiConfig' );
+const path = require('path');
+const deepAssign = require('deep-assign');
+const R = require('ramda');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlMinify = require('./util/config/minify');
+const CONFIG = require('./util/config');
 
-module.exports = webpackMultiConfig( 'dev' );
+
+const environment = process.env.NODE_ENV;
+// Consider `development` by default
+const isProd = environment === 'production';
+
+
+// assign :: Object -> Object -> Object
+const assign = R.curry((a, b) => deepAssign({}, a, b));
+// appendToArrayByPath :: Array -> Object -> Object -> Object
+const appendToArrayByPath = R.curry((path, data, object) =>
+  R.pipe(R.path(path), R.append(data), R.set(R.lensPath(path), R.__, object))(object)
+);
+
+// addLoader :: Object -> Object -> Object
+const addLoader = appendToArrayByPath(['module', 'loaders']);
+// addPlugin :: Object -> Object -> Object
+const addPlugin = appendToArrayByPath(['plugins']);
+
+const webpackConfig = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '',
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [],
+  },
+  plugins: [],
+};
+
+function addPugSupport(cfg) {
+  const loader = {
+    test: /\.pug$/,
+    loader: 'pug',
+    options: {
+      pretty: !isProd,
+    },
+  };
+  const plugin = new HtmlWebpackPlugin({
+    filename: 'html/index.html',
+    template: 'src/html/index.pug',
+    minify: isProd ? htmlMinify : false,
+  });
+  return R.pipe(addLoader(loader), addPlugin(plugin))(cfg);
+}
+
+function addBabelSupport(cfg) {
+  const loader = {
+    test: /\.js$/,
+    exclude: /(node_modules|public)/,
+    loader: 'babel',
+  };
+  return addLoader(loader)(cfg);
+}
+
+function makeConfig(cfg) {
+  let updatedConfig = cfg;
+
+  updatedConfig = addPugSupport(updatedConfig);
+  updatedConfig = addBabelSupport(updatedConfig);
+
+  return updatedConfig;
+}
+
+module.exports = makeConfig(webpackConfig);
