078b8e3dbbb5e0feb36932eb08b79f28bb778981
edloidas <edloidas@gmail.com>
2016-11-04 16:07:15 +0300

    Add js uglify webpack plugin.

---
+++ util/config/uglify.js
@@ -0,0 +1,2 @@
+// UglifyJS2 options
+module.exports = {/* default */};
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ Configurable webpack config.
 Uses $NODE_ENV, `production` or `development`
 */
 const path = require('path');
-const deepAssign = require('deep-assign');
 const R = require('ramda');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglify-js-plugin');
 const htmlMinify = require('./util/config/minify');
+const jsMinify = require('./util/config/uglify');
 const CONFIG = require('./util/config');
 
 
@@ -15,22 +16,20 @@ const environment = process.env.NODE_ENV;
 const isProd = environment === 'production';
 
 
-// assign :: Object -> Object -> Object
-const assign = R.curry((a, b) => deepAssign({}, a, b));
 // appendToArrayByPath :: Array -> Object -> Object -> Object
-const appendToArrayByPath = R.curry((path, data, object) =>
-  R.pipe(R.path(path), R.append(data), R.set(R.lensPath(path), R.__, object))(object)
+const appendToArrayByPath = R.curry((objPath, data, object) =>
+  R.pipe(R.path(objPath), R.append(data), R.set(R.lensPath(objPath), R.__, object))(object)
 );
-
 // addLoader :: Object -> Object -> Object
 const addLoader = appendToArrayByPath(['module', 'loaders']);
 // addPlugin :: Object -> Object -> Object
 const addPlugin = appendToArrayByPath(['plugins']);
 
+// Webpack config template
 const webpackConfig = {
   entry: './src/js/app.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, CONFIG.root.dist),
     publicPath: '',
     filename: 'app.js',
   },
@@ -40,6 +39,7 @@ const webpackConfig = {
   plugins: [],
 };
 
+// Allows to convert pug->html
 function addPugSupport(cfg) {
   const loader = {
     test: /\.pug$/,
@@ -56,13 +56,23 @@ function addPugSupport(cfg) {
   return R.pipe(addLoader(loader), addPlugin(plugin))(cfg);
 }
 
+// Removes Flow types with Babel
+// Uglifies code in production
 function addBabelSupport(cfg) {
   const loader = {
     test: /\.js$/,
     exclude: /(node_modules|public)/,
     loader: 'babel',
   };
-  return addLoader(loader)(cfg);
+
+  const devPlugins = [];
+  const prodPlugins = [
+    new UglifyJsPlugin(jsMinify),
+  ];
+  const plugins = isProd ? R.concat(devPlugins, prodPlugins) : devPlugins;
+
+  const addPlugins = list => R.map(addPlugin, list);
+  return R.pipe(addLoader(loader), ...addPlugins(plugins))(cfg);
 }
 
 function makeConfig(cfg) {
