dde5a37122b38ad712f65eaf9b45fcb50a8cd8e3
edloidas <edloidas@gmail.com>
2017-06-30 22:09:51 +0300

    :shirt: Remove conflicting ESLint rules
    Removed conflicting ESLint and Prettier rules.
    Added `prettier-check` script to check for conflicting rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,19 +12,10 @@ module.exports = {
     'prettier',
   ],
   'rules': {
-    'space-before-function-paren': [ 2, { 'anonymous': 'always', 'named': 'never' } ],
     'spaced-comment': [ 2, 'always', { 'exceptions': [ '-', '+' ] } ],
-    'arrow-spacing': [ 2, { 'before': true, 'after': true } ],
     'no-restricted-syntax': [ 'off' ],
     'object-property-newline': [ 'off', { 'allowMultiplePropertiesPerLine': true } ],
     'no-underscore-dangle': [ 'off' ],
-    'comma-dangle': [ 'error', {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'always-multiline',
-      'exports': 'always-multiline',
-      'functions': 'always-multiline',
-    }],
     'import/no-extraneous-dependencies': [ 'off', { 'devDependencies': [ 'util/', '**/*.test.js', '**/*.spec.js' ] } ],
     'react/jsx-filename-extension': [ 1, { 'extensions': [ '.js', '.jsx' ] } ],
     'prettier/prettier': ['error', {
