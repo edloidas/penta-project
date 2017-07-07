c8e4cca0f2e4165eaf5be6739f75d00489904335
edloidas <edloidas@gmail.com>
2017-07-08 00:11:36 +0300

    Update the router redirect to work in Electron with `file:` protocol
    Added path hack with redirect, to render the `/` path in Electron.
    Removed `pack` script.
    Added `try` script to open the app in electron for quick testing.

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
@@ -16,10 +16,12 @@ import configure from './store';
 const store = configure();
 const history = syncHistoryWithStore(createHistory({ basename: '/' }), store);
 
+// HACK: Redirect is needed to load the root `/` in Electron environment
 render(
   <Provider store={store}>
     <Router history={history}>
       <div>
+        {window.location.pathname.includes('index.html') && <Redirect to="/" />}
         <Route exact path="/" component={Start} />
         <Route path="/menu" component={MainMenu} />
       </div>
